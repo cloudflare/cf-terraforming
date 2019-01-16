34d092d587beee83a52bb3c3450c6fb169ab6916
Patryk Szczygłowski <patryk@patryk.net>
2019-01-16 17:15:10 +0000

    Zone Lockdown template

--- internal/app/cf-terraforming/cmd/zone_lockdown.go
+++ internal/app/cf-terraforming/cmd/zone_lockdown.go
@@ -5,8 +5,31 @@ import (
 	"log"
 	"os"
 
+	"text/template"
+
+	cloudflare "github.com/cloudflare/cloudflare-go"
 	"github.com/spf13/cobra"
 )
+
+const zoneLockdownTemplate = `
+resource "cloudflare_zone_lockdown" "{{replace .Zone.Name "." "_"}}_{{.Lockdown.ID}}" {
+    zone_id = "{{.Zone.ID}}"
+    description = "{{.Lockdown.Description}}"
+    urls = [
+{{range .Lockdown.URLs}}
+        "{{.}}",
+{{end}}
+    ]
+    configurations = [
+{{range .Lockdown.Configurations}}
+        {
+            target = "{{.Target}}"
+            value = "{{.Value}}"
+        },
+{{end}}
+    ]
+}
+`
 
 func init() {
 	rootCmd.AddCommand(zoneLockdownCmd)
@@ -35,10 +58,25 @@ var zoneLockdownCmd = &cobra.Command{
 
 				for _, r := range lockdowns.Result {
 					log.Printf("[DEBUG] Lockdown ID %s, URL %s\n", r.ID, r.URLs)
-					// TODO: Process
+					zoneLockdownParse(zone, r)
 				}
 			}
 
 		}
 	},
+}
+
+func zoneLockdownParse(zone cloudflare.Zone, lockdown cloudflare.ZoneLockdown) {
+	tmpl := template.Must(template.New("zone_lockdown").Funcs(templateFuncMap).Parse(zoneLockdownTemplate))
+	if err := tmpl.Execute(os.Stdout,
+		struct {
+			Zone     cloudflare.Zone
+			Lockdown cloudflare.ZoneLockdown
+		}{
+			Zone:     zone,
+			Lockdown: lockdown,
+		}); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }

5c12b52b912333c00c226767d2ceb4a98d58d206
Patryk Szczygłowski <patryk@patryk.net>
2019-01-16 15:56:58 +0000

    Generate "record" from template

--- internal/app/cf-terraforming/cmd/record.go
+++ internal/app/cf-terraforming/cmd/record.go
@@ -4,10 +4,46 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"text/template"
 
 	cloudflare "github.com/cloudflare/cloudflare-go"
 	"github.com/spf13/cobra"
 )
+
+const recordTemplate = `
+resource "cloudflare_record" "{{replace .Zone.Name "." "_"}}_{{replace .Record.Name "." "_"}}" {
+    domain = "{{.Zone.Name}}"
+{{ if .Zone.Paused}}
+    paused = "true"
+{{end}}
+    name = "{{.Record.Name}}"
+    type = "{{.Record.Type}}"
+    ttl = "{{.Record.TTL}}"
+    proxied = "{{.Record.Proxied}}"
+{{ if or (eq .Record.Type "MX") (eq .Record.Type "URI") }}
+    priority = "{{.Record.Priority}}"
+{{end}}
+{{ if .IsValueTypeField }}
+    value = "{{.Record.Content}}"
+{{end}}
+{{ if .IsDataTypeField }}
+    data {
+{{range $k, $v := .Record.Data}}
+        {{ $k }} = "{{ $v }}"
+{{end}}
+    }
+{{end}}
+}
+
+`
+
+var dnsTypeValueFields = []string{
+	"A", "AAAA", "CNAME", "NS", "MX", "TXT", "SPF",
+}
+
+var dnsTypeDataFields = []string{
+	"LOC", "SRV", "CERT", "DNSKEY", "DS", "NAPTR", "SMIMEA", "SSHFP", "TLSA", "URI",
+}
 
 func init() {
 	rootCmd.AddCommand(recordCmd)
@@ -31,7 +67,28 @@ var recordCmd = &cobra.Command{
 			}
 			for _, r := range recs {
 				fmt.Printf("Record ID %s, Name %s, Type %s: %s\n", r.ID, r.Name, r.Type, r.Content)
+
+				recordParse(zone, r)
 			}
 		}
 	},
+}
+
+func recordParse(zone cloudflare.Zone, record cloudflare.DNSRecord) {
+	tmpl := template.Must(template.New("record").Funcs(templateFuncMap).Parse(recordTemplate))
+	if err := tmpl.Execute(os.Stdout,
+		struct {
+			Zone             cloudflare.Zone
+			Record           cloudflare.DNSRecord
+			IsValueTypeField bool
+			IsDataTypeField  bool
+		}{
+			Zone:             zone,
+			Record:           record,
+			IsValueTypeField: contains(dnsTypeValueFields, record.Type),
+			IsDataTypeField:  contains(dnsTypeDataFields, record.Type),
+		}); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
--- internal/app/cf-terraforming/cmd/util.go
+++ internal/app/cf-terraforming/cmd/util.go
@@ -9,6 +9,16 @@ func replace(input, from, to string) string {
 	return strings.Replace(input, from, to, -1)
 }
 
+func contains(slice []string, item string) bool {
+	set := make(map[string]struct{}, len(slice))
+	for _, s := range slice {
+		set[s] = struct{}{}
+	}
+
+	_, ok := set[item]
+	return ok
+}
+
 var templateFuncMap = template.FuncMap{
 	"replace": replace,
 }

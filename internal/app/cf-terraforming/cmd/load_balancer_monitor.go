a48aebfd60414fda9c53907cd50f7d8cfde8317b
Zack Proser <zproser@cloudflare.com>
2019-02-13 11:35:59 -0800

    SDK-31 Implement load balancer monitor

---
+++ internal/app/cf-terraforming/cmd/load_balancer_monitor.go
@@ -0,0 +1,72 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	cloudflare "github.com/cloudflare/cloudflare-go"
+
+	"text/template"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+const loadBalancerMonitorTemplate = `
+resource "cloudflare_load_balancer_monitor" "{{.LBM.ID}}" {
+    expected_body = "{{.LBM.ExpectedBody}}"
+    expected_codes = "{{.LBM.ExpectedCodes}}"
+    method = "{{.LBM.Method}}"
+    timeout = {{.LBM.Timeout}}
+    path = "{{.LBM.Path}}"
+    interval = {{.LBM.Interval}}
+    retries = {{.LBM.Retries}}
+    description = "{{.LBM.Description}}"
+    {{if isMap .LBM.Header}}
+    header {
+    {{range $k, $v := .LBM.Header}}
+        {{$k}} = {{ quoteIfString $v }}
+    {{end}}
+    }
+    {{end}}
+}
+`
+
+func init() {
+	rootCmd.AddCommand(loadBalancerMonitorCmd)
+}
+
+var loadBalancerMonitorCmd = &cobra.Command{
+	Use:   "load_balancer_monitor",
+	Short: "Import a load balancer monitor into Terraform",
+	Run: func(cmd *cobra.Command, args []string) {
+		loadBalancerMonitors, err := api.ListLoadBalancerMonitors()
+
+		if err != nil {
+			log.Fatal(err)
+			os.Exit(1)
+		}
+
+		if len(loadBalancerMonitors) > 0 {
+			for _, lbm := range loadBalancerMonitors {
+
+				log.WithFields(logrus.Fields{
+					"ID":          lbm.ID,
+					"Description": lbm.Description,
+				}).Debug("Processing load balancer monitor")
+
+				loadBalancerMonitorParse(lbm)
+			}
+		}
+	},
+}
+
+func loadBalancerMonitorParse(lbm cloudflare.LoadBalancerMonitor) {
+	tmpl := template.Must(template.New("load_balancer_monitor").Funcs(templateFuncMap).Parse(loadBalancerMonitorTemplate))
+	tmpl.Execute(os.Stdout,
+		struct {
+			LBM cloudflare.LoadBalancerMonitor
+		}{
+			LBM: lbm,
+		})
+}

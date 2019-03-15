d7a0f06b38720f22006ad015bdb8b9e42718fedf
Patryk Szczygłowski <patryk@patryk.net>
2019-03-15 15:43:51 +0000

    Support for importing state for cloudflare_zone

--- internal/app/cf-terraforming/cmd/zone.go
+++ internal/app/cf-terraforming/cmd/zone.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"os"
+	"strconv"
 
 	"text/template"
 
@@ -18,6 +19,23 @@ resource "cloudflare_zone" "{{replace .Zone.Name "." "_"}}" {
 }
 
 `
+
+type ZoneAttributes struct {
+	ID                   string `json:"id"`
+	PhishingDetected     string `json:"meta.phishing_detected"`
+	WildcardProxiable    string `json:"meta.wildcard_proxiable"`
+	NameServersNum       string `json:"name_servers.#"`
+	NameServers0         string `json:"name_servers.0"`
+	NameServers1         string `json:"name_servers.1"`
+	Paused               string `json:"paused"`
+	Plan                 string `json:"plan"`
+	Status               string `json:"status"`
+	Type                 string `json:"type"`
+	VanityNameServersNum string `json:"vanity_name_servers.#"`
+	VanityNameServers0   string `json:"vanity_name_servers.0"`
+	VanityNameServers1   string `json:"vanity_name_servers.1"`
+	Zone                 string `json:"zone"`
+}
 
 // we enforce the use of the Cloudflare API 'legacy_id' field until the mapping of plan is fixed in cloudflare-go
 const (
@@ -58,7 +76,12 @@ var zoneCmd = &cobra.Command{
 				"Name": zoneDetails.Name,
 			}).Debug("Processing zone")
 
-			zoneParse(zone)
+			if tfstate {
+				r := zoneResourceStateBuild(zone)
+				resourcesMap["cloudflare_zone."+zone.ID] = r
+			} else {
+				zoneParse(zone)
+			}
 		}
 	},
 }
@@ -73,4 +96,42 @@ func zoneParse(zone cloudflare.Zone) {
 			Zone:     zone,
 			ZonePlan: idForName[zone.Plan.Name],
 		})
+}
+
+func zoneResourceStateBuild(zone cloudflare.Zone) Resource {
+	r := Resource{
+		Primary: Primary{
+			Id: zone.ID,
+			Attributes: ZoneAttributes{
+				ID:                   zone.ID,
+				PhishingDetected:     strconv.FormatBool(zone.Meta.PhishingDetected),
+				WildcardProxiable:    strconv.FormatBool(zone.Meta.WildcardProxiable),
+				NameServersNum:       strconv.Itoa(len(zone.NameServers)),
+				NameServers0:         zone.NameServers[0],
+				NameServers1:         zone.NameServers[1],
+				Paused:               strconv.FormatBool(zone.Paused),
+				Plan:                 idForName[zone.Plan.Name],
+				Status:               zone.Status,
+				VanityNameServersNum: strconv.Itoa(len(zone.VanityNS)),
+				Type:                 zone.Type,
+				Zone:                 zone.Name,
+			},
+			Meta:    make(map[string]string),
+			Tainted: false,
+		},
+		DependsOn: []string{},
+		Deposed:   []string{},
+		Provider:  "provider.cloudflare",
+		Type:      "cloudflare_zone",
+	}
+
+	zoneAttributes := r.Primary.Attributes.(ZoneAttributes)
+	if len(zone.VanityNS) > 0 {
+		zoneAttributes.VanityNameServers0 = zone.VanityNS[0]
+	}
+	if len(zone.VanityNS) > 1 {
+		zoneAttributes.VanityNameServers1 = zone.VanityNS[0]
+	}
+
+	return r
 }

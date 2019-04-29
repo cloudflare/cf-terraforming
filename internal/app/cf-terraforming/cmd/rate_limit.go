2e4b8514bca0c494c89b80bfaf0d685f51479ddb
Patryk Szczygłowski <patryk@patryk.net>
2019-04-29 16:50:35 +0100

    Implement ftstate for `cloudflare_rate_limit`

--- internal/app/cf-terraforming/cmd/rate_limit.go
+++ internal/app/cf-terraforming/cmd/rate_limit.go
@@ -1,11 +1,13 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"text/template"
 
 	cloudflare "github.com/cloudflare/cloudflare-go"
+	"github.com/hashicorp/terraform/helper/hashcode"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
@@ -88,7 +90,13 @@ var rateLimitCmd = &cobra.Command{
 						"Description": r.Description,
 					}).Debug("Processing rate limit")
 
-					rateLimitParse(zone, r)
+					if tfstate {
+						r := rateLimitResourceStateBuild(zone, r)
+						resourcesMap["cloudflare_rate_limit."+r.Primary.Id] = r
+					} else {
+						rateLimitParse(zone, r)
+					}
+
 				}
 			}
 		}
@@ -105,4 +113,73 @@ func rateLimitParse(zone cloudflare.Zone, rateLimit cloudflare.RateLimit) {
 			Zone:      zone,
 			RateLimit: rateLimit,
 		})
+}
+
+func rateLimitResourceStateBuild(zone cloudflare.Zone, rateLimit cloudflare.RateLimit) Resource {
+	r := Resource{
+		Primary: Primary{
+			Id: rateLimit.ID,
+			Attributes: map[string]string{
+				"id":                                rateLimit.ID,
+				"action.#":                          "1",
+				"action.0.mode":                     rateLimit.Action.Mode,
+				"action.0.timeout":                  fmt.Sprint(rateLimit.Action.Timeout),
+				"bypass_url_patterns.#":             fmt.Sprint(len(rateLimit.Bypass)),
+				"description":                       rateLimit.Description,
+				"disabled":                          fmt.Sprint(rateLimit.Disabled),
+				"match.#":                           "1",
+				"match.0.request.#":                 "1",
+				"match.0.request.0.methods.#":       fmt.Sprint(len(rateLimit.Match.Request.Methods)),
+				"match.0.request.0.schemes.#":       fmt.Sprint(len(rateLimit.Match.Request.Schemes)),
+				"match.0.request.0.url_pattern":     rateLimit.Match.Request.URLPattern,
+				"match.0.response.#":                "1",
+				"match.0.response.0.origin_traffic": fmt.Sprint(*rateLimit.Match.Response.OriginTraffic),
+				"match.0.response.0.statuses.#":     fmt.Sprint(len(rateLimit.Match.Response.Statuses)),
+				"period":                            fmt.Sprint(rateLimit.Period),
+				"threshold":                         fmt.Sprint(rateLimit.Threshold),
+				"zone":                              zone.Name,
+				"zone_id":                           zone.ID,
+			},
+			Meta:    make(map[string]string),
+			Tainted: false,
+		},
+		DependsOn: []string{},
+		Deposed:   []string{},
+		Provider:  "provider.cloudflare",
+		Type:      "cloudflare_rate_limit",
+	}
+
+	attributes := r.Primary.Attributes.(map[string]string)
+
+	if rateLimit.Action.Response != nil {
+		attributes["action.0.response.#"] = "1"
+		attributes["action.0.response.0.body"] = rateLimit.Action.Response.Body
+		attributes["action.0.response.0.content_type"] = rateLimit.Action.Response.ContentType
+	} else {
+		attributes["action.0.response.#"] = "0"
+	}
+
+	for _, bypass := range rateLimit.Bypass {
+		hash := hashcode.String(fmt.Sprintf("%s;", bypass.Value))
+		attributes[fmt.Sprintf("bypass_url_patterns.%d", hash)] = bypass.Value
+	}
+
+	for _, method := range rateLimit.Match.Request.Methods {
+		hash := hashcode.String(fmt.Sprintf("%s;", method))
+		attributes[fmt.Sprintf("match.0.request.0.methods.%d", hash)] = method
+	}
+
+	for _, scheme := range rateLimit.Match.Request.Schemes {
+		hash := hashcode.String(fmt.Sprintf("%s;", scheme))
+		attributes[fmt.Sprintf("match.0.request.0.schemes.%d", hash)] = scheme
+	}
+
+	for _, status := range rateLimit.Match.Response.Statuses {
+		hash := hashcode.String(fmt.Sprintf("%s;", fmt.Sprint(status)))
+		attributes[fmt.Sprintf("match.0.response.0.statuses.%d", hash)] = fmt.Sprint(status)
+	}
+
+	r.Primary.Attributes = attributes
+
+	return r
 }

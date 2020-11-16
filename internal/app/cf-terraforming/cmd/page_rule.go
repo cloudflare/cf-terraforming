e56c26abef4e66984ee963c8c1cffdfcd3750ae8
Patryk Szczygłowski <patryk@patryk.net>
2020-11-16 17:25:28 +0000

    SDK-106: page rules: proper rendering for cache_key_fields and cache_ttl_by_status fields

--- internal/app/cf-terraforming/cmd/page_rule.go
+++ internal/app/cf-terraforming/cmd/page_rule.go
@@ -23,9 +23,25 @@ resource "cloudflare_page_rule" "page_rule_{{.Rule.ID}}" {
     {{- if isMap .Value}}
         {{.ID}} {
         {{- range $k, $v := .Value }}
-            {{$k}} = {{ quoteIfString $v -}}
-        {{end }}
+            {{- if isSlice $v }}
+                {{- $k }} = [ {{ range $v }}"{{.}}", {{ end }} ]
+            {{- else if isMap $v }}
+                {{ $k }} {
+                {{- range $k1, $v1 := $v}}
+                    {{- if isSlice $v1 }}
+                        {{ $k1 }} = [ {{ range $v1 }}"{{.}}", {{ end }} ]
+                    {{- else }}
+                        {{$k1}} = {{ quoteIfString $v1 -}}
+                    {{- end }}
+                {{- end }}
+                }
+            {{- else }}
+                {{$k}} = {{ quoteIfString $v -}}
+            {{- end }}
+        {{- end }}
         }
+    {{ else if isSlice .Value}}
+        {{- .ID }} = [ {{ range .Value }}"{{.}}", {{ end }} ]
     {{else}}
         {{.ID}} = {{ quoteIfString .Value }}
     {{end -}}
@@ -66,6 +82,57 @@ var pageRuleCmd = &cobra.Command{
 					"Priority": rule.Priority,
 					"Status":   rule.Status,
 				}).Debug("Processing page rule")
+
+				actionsProcessed := make([]cloudflare.PageRuleAction, 0)
+				for _, action := range rule.Actions {
+
+					if action.ID == "cache_key_fields" {
+						for fieldID, fieldValue := range action.Value.(map[string]interface{}) {
+							if fieldID == "query_string" {
+								fieldMap := fieldValue.(map[string]interface{})
+
+								if fieldMap["exclude"] == "*" {
+									fieldMap["exclude"] = []interface{}{}
+									fieldMap["ignore"] = true
+								}
+
+								if fieldMap["include"] == "*" {
+									fieldMap["include"] = []interface{}{}
+									fieldMap["ignore"] = false
+								}
+							}
+						}
+
+						actionsProcessed = append(actionsProcessed, action)
+
+					} else if action.ID == "cache_ttl_by_status" {
+						for statusCodes, statusTTL := range action.Value.(map[string]interface{}) {
+							entry := map[string]interface{}{"codes": statusCodes}
+
+							switch value := statusTTL.(type) {
+							case float64:
+								entry["ttl"] = int32(value)
+							case string:
+								switch value {
+								case "no-cache":
+									entry["ttl"] = 0
+								case "no-store":
+									entry["ttl"] = -1
+								}
+							}
+
+							actionsProcessed = append(actionsProcessed, cloudflare.PageRuleAction{
+								ID:    action.ID,
+								Value: entry,
+							})
+						}
+
+					} else {
+						actionsProcessed = append(actionsProcessed, action)
+					}
+
+					rule.Actions = actionsProcessed
+				}
 
 				if tfstate {
 					// TODO: Implement state dump

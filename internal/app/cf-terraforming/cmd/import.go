19f29082966572a9b96d7c4f53e1289ef33f9b97
Jacob Bednarz <jacob.bednarz@gmail.com>
2021-04-01 11:12:43 +1100

    import: add the remainder of importable resources

--- internal/app/cf-terraforming/cmd/import.go
+++ internal/app/cf-terraforming/cmd/import.go
@@ -14,17 +14,25 @@ import (
 // resourceImportStringFormats contains a mapping of the resource type to the
 // composite ID that is compatible with performing an import.
 var resourceImportStringFormats = map[string]string{
-	"cloudflare_account_member":   ":account_id/:id",
-	"cloudflare_argo_tunnel":      ":account_id/:id",
-	"cloudflare_byo_ip_prefix":    ":id",
-	"cloudflare_certificate_pack": ":zone_id/:id",
-	"cloudflare_filter":           ":zone_id/:id",
-	"cloudflare_firewall_rule":    ":zone_id/:id",
-	"cloudflare_custom_hostname":  ":zone_id/:id",
-	"cloudflare_ip_list":          ":account_id/:id",
-	"cloudflare_record":           ":zone_id/:id",
-	"cloudflare_worker_route":     ":zone_id/:id",
-	"cloudflare_zone":             ":zone_id",
+	"cloudflare_access_rule":           ":identifer_type/:identifer_value/:id",
+	"cloudflare_account_member":        ":account_id/:id",
+	"cloudflare_argo_tunnel":           ":account_id/:id",
+	"cloudflare_byo_ip_prefix":         ":id",
+	"cloudflare_certificate_pack":      ":zone_id/:id",
+	"cloudflare_custom_pages":          ":identifer_type/:identifer_value/:id",
+	"cloudflare_filter":                ":zone_id/:id",
+	"cloudflare_firewall_rule":         ":zone_id/:id",
+	"cloudflare_custom_hostname":       ":zone_id/:id",
+	"cloudflare_custom_ssl":            ":zone_id/:id",
+	"cloudflare_ip_list":               ":account_id/:id",
+	"cloudflare_origin_ca_certificate": ":id",
+	"cloudflare_rate_limit":            ":zone_id/:id",
+	"cloudflare_record":                ":zone_id/:id",
+	"cloudflare_spectrum_application":  ":zone_id/:id",
+	"cloudflare_waf_override":          ":zone_id/:id",
+	"cloudflare_worker_route":          ":zone_id/:id",
+	"cloudflare_zone":                  ":zone_id",
+	"cloudflare_zone_lockdown":         ":zone_id/:id",
 }
 
 func init() {
@@ -41,49 +49,6 @@ func runImport() func(cmd *cobra.Command, args []string) {
 	return func(cmd *cobra.Command, args []string) {
 		var jsonStructData []interface{}
 		switch resourceType {
-		case "cloudflare_access_identity_provider":
-			if accountID != "" {
-				jsonPayload, err := api.AccessIdentityProviders(context.Background(), accountID)
-				if err != nil {
-					log.Fatal(err)
-				}
-
-				m, _ := json.Marshal(jsonPayload)
-				json.Unmarshal(m, &jsonStructData)
-			} else {
-				jsonPayload, err := api.ZoneLevelAccessIdentityProviders(context.Background(), zoneID)
-				if err != nil {
-					log.Fatal(err)
-				}
-
-				m, _ := json.Marshal(jsonPayload)
-				json.Unmarshal(m, &jsonStructData)
-			}
-		case "cloudflare_access_service_token":
-			if accountID != "" {
-				jsonPayload, _, err := api.AccessServiceTokens(context.Background(), accountID)
-				if err != nil {
-					log.Fatal(err)
-				}
-
-				m, _ := json.Marshal(jsonPayload)
-				json.Unmarshal(m, &jsonStructData)
-			} else {
-				jsonPayload, _, err := api.ZoneLevelAccessServiceTokens(context.Background(), zoneID)
-				if err != nil {
-					log.Fatal(err)
-				}
-
-				m, _ := json.Marshal(jsonPayload)
-				json.Unmarshal(m, &jsonStructData)
-			}
-		case "cloudflare_access_mutual_tls_certificate":
-			jsonPayload, err := api.AccessMutualTLSCertificates(context.Background(), accountID)
-			if err != nil {
-				log.Fatal(err)
-			}
-			m, _ := json.Marshal(jsonPayload)
-			json.Unmarshal(m, &jsonStructData)
 		case "cloudflare_access_rule":
 			if accountID != "" {
 				jsonPayload, err := api.ListAccountAccessRules(context.Background(), accountID, cloudflare.AccessRule{}, 1)
@@ -192,11 +157,43 @@ func runImport() func(cmd *cobra.Command, args []string) {
 			}
 			m, _ := json.Marshal(jsonPayload)
 			json.Unmarshal(m, &jsonStructData)
+		case "cloudflare_origin_ca_certificate":
+			jsonPayload, err := api.OriginCertificates(context.Background(), cloudflare.OriginCACertificateListOptions{ZoneID: zoneID})
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			m, _ := json.Marshal(jsonPayload)
+			json.Unmarshal(m, &jsonStructData)
+		case "cloudflare_rate_limit":
+			jsonPayload, _, err := api.ListRateLimits(context.Background(), zoneID, cloudflare.PaginationOptions{})
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			m, _ := json.Marshal(jsonPayload)
+			json.Unmarshal(m, &jsonStructData)
 		case "cloudflare_record":
 			jsonPayload, err := api.DNSRecords(context.Background(), zoneID, cloudflare.DNSRecord{})
 			if err != nil {
 				log.Fatal(err)
 			}
+			m, _ := json.Marshal(jsonPayload)
+			json.Unmarshal(m, &jsonStructData)
+		case "cloudflare_spectrum_application":
+			jsonPayload, err := api.SpectrumApplications(context.Background(), zoneID)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			m, _ := json.Marshal(jsonPayload)
+			json.Unmarshal(m, &jsonStructData)
+		case "cloudflare_waf_override":
+			jsonPayload, err := api.ListWAFOverrides(context.Background(), zoneID)
+			if err != nil {
+				log.Fatal(err)
+			}
+
 			m, _ := json.Marshal(jsonPayload)
 			json.Unmarshal(m, &jsonStructData)
 		case "cloudflare_waf_package":
@@ -221,6 +218,14 @@ func runImport() func(cmd *cobra.Command, args []string) {
 			}
 			m, _ := json.Marshal(jsonPayload)
 			json.Unmarshal(m, &jsonStructData)
+		case "cloudflare_zone_lockdown":
+			jsonPayload, err := api.ListZoneLockdowns(context.Background(), zoneID, 1)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			m, _ := json.Marshal(jsonPayload.Result)
+			json.Unmarshal(m, &jsonStructData)
 		default:
 			fmt.Fprintf(cmd.OutOrStdout(), "%q is not yet supported for state import", resourceType)
 			return
@@ -240,9 +245,22 @@ func buildCompositeID(resourceType, resourceID string) string {
 		log.Fatalf("%s does not have an import format defined", resourceType)
 	}
 
+	var identiferType string
+	var identiferValue string
+
+	if accountID != "" {
+		identiferType = "account"
+		identiferValue = accountID
+	} else {
+		identiferType = "zone"
+		identiferValue = zoneID
+	}
+
 	s := ""
 	s += fmt.Sprintf("%s %s.%s_%s %s", terraformImportCmdPrefix, resourceType, terraformResourceNamePrefix, resourceID, resourceImportStringFormats[resourceType])
 	replacer := strings.NewReplacer(
+		":identifer_type", identiferType,
+		":identifer_value", identiferValue,
 		":zone_id", zoneID,
 		":account_id", accountID,
 		":id", resourceID,

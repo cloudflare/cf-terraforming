463a9ceee30e93190cb2aaa19e87df078482d3bc
Garet Wirth <gwirth@ookla.com>
2021-05-11 12:30:20 -0700

    Add deeper test for page_rules

--- testdata/terraform/cloudflare_page_rule.tf
+++ testdata/terraform/cloudflare_page_rule.tf
@@ -4,11 +4,40 @@ resource "cloudflare_page_rule" "terraform_managed_resource" {
   target = "*example.com/images/*"
   zone_id = "0da42c8d2132a9ddaf714f9e7c920711"
   actions {
-    minify {
-      css = "on"
-      html = "on"
-      js = "off"
+    cache_key_fields {
+      cookie {
+        check_presence = [ "x-some-header" ]
+        include = [ "x-some-other-header", "x-some-value" ]
+      }
+      header {
+        check_presence = [ "x-forwarded-for" ]
+        include = [ "authorization" ]
+      }
+      host {
+        resolved = true
+      }
+      query_string {
+        exclude = [ "*" ]
+      }
+      user {
+        device_type = true
+        geo = true
+        lang = false
+      }
     }
-    always_online = "on"
+    cache_ttl_by_status {
+      codes = "200"
+      ttl = 60
+    }
+    cache_ttl_by_status {
+      codes = "204"
+      ttl = -1
+    }
+    cache_ttl_by_status {
+      codes = "300-399"
+      ttl = 0
+    }
+    browser_cache_ttl = 1800
+    host_header_override = "not-example.com"
   }
 }

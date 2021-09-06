2c183f6b3908f38317b79be92bf356b3f6736cce
Jacob Bednarz <jacob.bednarz@gmail.com>
2021-09-07 09:02:10 +1000

    tests: fix `cloudflare_zone_lockdown` assertion

--- testdata/terraform/cloudflare_zone_lockdown.tf
+++ testdata/terraform/cloudflare_zone_lockdown.tf
@@ -3,4 +3,8 @@ resource "cloudflare_zone_lockdown" "terraform_managed_resource" {
   paused = false
   urls = [ "api.mysite.com/some/endpoint*" ]
   zone_id = "0da42c8d2132a9ddaf714f9e7c920711"
+  configurations {
+    target = "ip"
+    value = "198.51.100.4"
+  }
 }

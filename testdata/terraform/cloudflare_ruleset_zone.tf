18de85c6eda9cb2ad5ca212977e86682ed5777a2
Sachin Fernandes <sachin@cloudflare.com>
2022-04-12 19:06:19 -0700

    Add more ruleset test cases

--- testdata/terraform/cloudflare_ruleset_zone.tf
+++ testdata/terraform/cloudflare_ruleset_zone.tf
@@ -169,6 +169,17 @@ resource "cloudflare_ruleset" "terraform_managed_resource" {
         name      = "example-http-header-2"
         operation = "remove"
       }
+      headers {
+        expression = "(ip.geoip.continent eq \"pluto\")"
+        name       = "example-http-header-3"
+        operation  = "set"
+        value      = "space-header"
+      }
+      uri {
+        path {
+          value = "/aquarii_b"
+        }
+      }
     }
   }
   rules {
@@ -204,4 +215,12 @@ resource "cloudflare_ruleset" "terraform_managed_resource" {
       }
     }
   }
+}
+
+resource "cloudflare_ruleset" "terraform_managed_resource" {
+  description = "Some ruleset"
+  kind        = "zone"
+  name        = "default"
+  phase       = "http_request_late_transform"
+  zone_id     = "0da42c8d2132a9ddaf714f9e7c920711"
 }

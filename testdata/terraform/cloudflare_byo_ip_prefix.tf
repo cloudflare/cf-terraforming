e736464551f476db0c181e6e37afb6e57131ace4
Jacob Bednarz <jacob.bednarz@gmail.com>
2022-04-28 05:37:22 +1000

    fix output assertions

--- testdata/terraform/cloudflare_byo_ip_prefix.tf
+++ testdata/terraform/cloudflare_byo_ip_prefix.tf
@@ -1,4 +1,5 @@
 resource "cloudflare_byo_ip_prefix" "terraform_managed_resource" {
+  account_id    = "f037e56e89293a057740de681ac9abbe"
   advertisement = "on"
   description   = "Internal test prefix"
   prefix_id     = "9a7806061c88ada191ed06f989cc3dac"

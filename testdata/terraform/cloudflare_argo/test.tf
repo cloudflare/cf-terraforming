296b39b62766552d4551af020fd88dc2e14b4c86
Tamas Jozsa <tamas@cloudflare.com>
2023-01-24 11:51:50 +0000

    fix Argo Smart Routing overriding Tiered Caching

--- testdata/terraform/cloudflare_argo/test.tf
+++ testdata/terraform/cloudflare_argo/test.tf
@@ -1,5 +1,5 @@
 resource "cloudflare_argo" "terraform_managed_resource" {
-  smart_routing  = "off"
+  smart_routing  = "on"
   tiered_caching = "off"
   zone_id        = "0da42c8d2132a9ddaf714f9e7c920711"
 }

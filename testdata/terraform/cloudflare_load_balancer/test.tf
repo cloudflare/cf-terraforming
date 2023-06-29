34f760c68b707ac43e07db8963a61b76e55970ca
Jacob Bednarz <jacob.bednarz@gmail.com>
2023-06-29 15:07:34 +1000

    generate/load_balancer: correctly remap `country_pools`, `region_pools` and `pop_pools`
    Updates the `cloudflare_load_balancer` generation to remap the
    `country_pools`, `region_pools` and `pop_pools` to the nested block
    instead of the object/map field.
    
    This was a bit of tricky one to track down because the original response
    from the API _has_ the correct schema attributes but the kicker is that
    they are not a supported type (for good reasons!).

--- testdata/terraform/cloudflare_load_balancer/test.tf
+++ testdata/terraform/cloudflare_load_balancer/test.tf
@@ -10,6 +10,38 @@ resource "cloudflare_load_balancer" "terraform_managed_resource" {
   steering_policy      = "dynamic_latency"
   ttl                  = 30
   zone_id              = "0da42c8d2132a9ddaf714f9e7c920711"
+  country_pools {
+    country  = "AU"
+    pool_ids = ["de90f38ced07c2e2f4df50b1f61d4194", "9290f38c5d07c2e2f4df57b1f61d4196"]
+  }
+  country_pools {
+    country  = "ME"
+    pool_ids = ["00920f38ce07c2e2f4df50b1f61d4194"]
+  }
+  country_pools {
+    country  = "US"
+    pool_ids = ["abd90f38ced07c2e2f4df50b1f61d4194", "f9138c5d07c2e2f4df57b1f61d4196"]
+  }
+  pop_pools {
+    pool_ids = ["de90f38ced07c2e2f4df50b1f61d4194", "9290f38c5d07c2e2f4df57b1f61d4196"]
+    pop      = "LAX"
+  }
+  pop_pools {
+    pool_ids = ["abd90f38ced07c2e2f4df50b1f61d4194", "f9138c5d07c2e2f4df57b1f61d4196"]
+    pop      = "LHR"
+  }
+  pop_pools {
+    pool_ids = ["00920f38ce07c2e2f4df50b1f61d4194"]
+    pop      = "SJC"
+  }
+  region_pools {
+    pool_ids = ["00920f38ce07c2e2f4df50b1f61d4194"]
+    region   = "ENAM"
+  }
+  region_pools {
+    pool_ids = ["de90f38ced07c2e2f4df50b1f61d4194", "9290f38c5d07c2e2f4df57b1f61d4196"]
+    region   = "WNAM"
+  }
   session_affinity_attributes {
     drain_duration = 100
     samesite       = "Auto"

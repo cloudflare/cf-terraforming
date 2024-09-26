45521e1d99b1c69baf9e7b7b57cb432ec528b3b6
Jacob Bednarz <jacob.bednarz@hey.com>
2024-09-26 14:33:34 +1000

    reorder attributes

--- testdata/terraform/cloudflare_bot_management/test.tf
+++ testdata/terraform/cloudflare_bot_management/test.tf
@@ -1,10 +1,10 @@
 resource "cloudflare_bot_management" "terraform_managed_resource" {
+  ai_bots_protection              = "block"
   enable_js                       = true
   optimize_wordpress              = true
   sbfm_definitely_automated       = "block"
   sbfm_likely_automated           = "managed_challenge"
   sbfm_static_resource_protection = false
   sbfm_verified_bots              = "allow"
-  ai_bots_protection              = "block"
   zone_id                         = "0da42c8d2132a9ddaf714f9e7c920711"
 }

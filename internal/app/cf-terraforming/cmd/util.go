994de731353f47e18cd428a108b28eb8438c487f
Musa Jundi <musa@cloudflare.com>
2025-02-27 14:39:06 -0600

    Fix type assertion error and typo

--- internal/app/cf-terraforming/cmd/util.go
+++ internal/app/cf-terraforming/cmd/util.go
@@ -226,7 +226,7 @@ func writeAttrLine(key string, value interface{}, parentName string, body *hclwr
 		for _, item := range value.([]map[string]interface{}) {
 			mapCty := make(map[string]cty.Value)
 			for k, v := range item {
-				mapCty[k] = cty.StringVal(v.(string))
+				mapCty[k] = cty.StringVal(fmt.Sprintf("%v", v))
 			}
 			childCty = append(childCty, cty.MapVal(mapCty))
 		}
@@ -328,7 +328,7 @@ func transformToCollection(value string) string {
 }
 
 // modifyResponsePayload takes the current resource and the `gjson.Result`
-// to run arbitary modifications to the JSON before passing it to be overlayed
+// to run arbitrary modifications to the JSON before passing it to be overlayed
 // the provider schema.
 func modifyResponsePayload(resourceName string, value gjson.Result) string {
 	output := value.String()

a30efe84b693d7007c9f6fa7676a15bfbd146f16
Vaishak Dinesh <vaishak@cloudflare.com>
2025-03-06 12:55:51 -0800

    fix: update data

--- testdata/terraform/v5/cloudflare_account_member/test.tf
+++ testdata/terraform/v5/cloudflare_account_member/test.tf
@@ -1,6 +1,6 @@
 resource "cloudflare_account_member" "terraform_managed_resource" {
   account_id = "f037e56e89293a057740de681ac9abbe"
-  email      = "ggalow@cloudflare.com"
+  email      = "jdoe@cloudflare.com"
   roles      = ["33666b9c79b9a5273fc7344ff42f953d"]
   status     = "accepted"
 }

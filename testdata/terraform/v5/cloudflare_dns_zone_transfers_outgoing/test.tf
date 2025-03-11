c01a6fbbc6f1b9c121b3cc4d5e0f5df4c891c68b
Vaishak Dinesh <vaishak@cloudflare.com>
2025-03-11 13:49:36 -0700

    fix: transfers outgoing

--- testdata/terraform/v5/cloudflare_dns_zone_transfers_outgoing/test.tf
+++ testdata/terraform/v5/cloudflare_dns_zone_transfers_outgoing/test.tf
@@ -1,5 +1,6 @@
 resource "cloudflare_dns_zone_transfers_outgoing" "terraform_managed_resource" {
-  zone_id = "269d8f4853475ca241c4e730be286b20"
-  name = "www.example.com."
-  peers = ["23ff594956f20c2a721606e94745a8aa", "00920f38ce07c2e2f4df50b1f61d4194"]
+  name    = "terraform.cfapi.net."
+  peers   = ["226c39f30555498082ba9c4a3204e1b8"]
+  zone_id = "0da42c8d2132a9ddaf714f9e7c920711"
 }
+

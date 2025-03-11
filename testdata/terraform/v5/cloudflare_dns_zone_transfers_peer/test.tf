6521e3212c727e8b4e2207e5efbddbc036955fc4
Vaishak Dinesh <vaishak@cloudflare.com>
2025-03-11 13:49:50 -0700

    fix: transfers peer

--- testdata/terraform/v5/cloudflare_dns_zone_transfers_peer/test.tf
+++ testdata/terraform/v5/cloudflare_dns_zone_transfers_peer/test.tf
@@ -1,4 +1,29 @@
-resource "cloudflare_dns_zone_transfers_peer" "terraform_managed_resource" {
-  account_id = "01a7362d577a6c3019a474fd6f485823"
-  name = "my-peer-1"
+resource "cloudflare_dns_zone_transfers_peer" "terraform_managed_resource_0" {
+  account_id  = "f037e56e89293a057740de681ac9abbe"
+  ip          = "1.2.3.4"
+  ixfr_enable = false
+  name        = "terraform-peer"
+  port        = 53
 }
+
+resource "cloudflare_dns_zone_transfers_peer" "terraform_managed_resource_1" {
+  account_id  = "f037e56e89293a057740de681ac9abbe"
+  ixfr_enable = false
+  name        = "terraform-peer"
+  port        = 0
+}
+
+resource "cloudflare_dns_zone_transfers_peer" "terraform_managed_resource_2" {
+  account_id  = "f037e56e89293a057740de681ac9abbe"
+  ixfr_enable = false
+  name        = "terraform-peer"
+  port        = 0
+}
+
+resource "cloudflare_dns_zone_transfers_peer" "terraform_managed_resource_3" {
+  account_id  = "f037e56e89293a057740de681ac9abbe"
+  ixfr_enable = false
+  name        = "fcusoidvut"
+  port        = 0
+}
+

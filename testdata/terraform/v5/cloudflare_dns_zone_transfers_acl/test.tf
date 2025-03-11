0f1e1c81f6fb210e1eaf686dbc5c5f0263d2414c
Vaishak Dinesh <vaishak@cloudflare.com>
2025-03-11 13:49:09 -0700

    fix: transfers acl

--- testdata/terraform/v5/cloudflare_dns_zone_transfers_acl/test.tf
+++ testdata/terraform/v5/cloudflare_dns_zone_transfers_acl/test.tf
@@ -1,5 +1,6 @@
 resource "cloudflare_dns_zone_transfers_acl" "terraform_managed_resource" {
-  account_id = "01a7362d577a6c3019a474fd6f485823"
-  ip_range = "192.0.2.53/28"
-  name = "my-acl-1"
+  account_id = "f037e56e89293a057740de681ac9abbe"
+  ip_range   = "1.2.3.4/32"
+  name       = "jsjzqpgumk"
 }
+

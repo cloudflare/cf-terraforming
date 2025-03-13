e389a3341a37fd165123c80a1e67e95d2be1e8d7
Vaishak Dinesh <vaishak@cloudflare.com>
2025-03-13 10:53:17 -0700

    feat: post process hcl

--- internal/app/cf-terraforming/cmd/generate.go
+++ internal/app/cf-terraforming/cmd/generate.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"io"
 	"net/http"
 	"os"
@@ -1555,6 +1556,7 @@ func generateResources() func(cmd *cobra.Command, args []string) {
 				f.Body().AppendNewline()
 			}
 
+			postProcess(f, resourceType)
 			tfOutput := string(hclwrite.Format(f.Bytes()))
 			fmt.Fprint(cmd.OutOrStdout(), tfOutput)
 		}
@@ -1588,4 +1590,52 @@ func unMarshallJSONStructData(modifiedJSONString string) ([]interface{}, error) 
 		return dataSlice, nil
 	}
 	return []interface{}{data}, nil
+}
+
+// postProcess allows you to perform additional actions on the generated hcl
+func postProcess(f *hclwrite.File, resourceType string) {
+	switch resourceType {
+	case "cloudflare_stream_live_input", "cloudflare_stream":
+		addJSONEncode(f, "meta")
+	}
+}
+
+func addJSONEncode(f *hclwrite.File, attributeName string) {
+	for _, block := range f.Body().Blocks() {
+		if block.Type() != "resource" {
+			continue
+		}
+		if len(block.Labels()) < 1 {
+			continue
+		}
+		if block.Labels()[0] != resourceType {
+			continue
+		}
+		body := block.Body()
+		attr := body.GetAttribute(attributeName)
+		if attr == nil {
+			continue
+		}
+		exprTokens := attr.Expr().BuildTokens(nil)
+		exprText := string(exprTokens.Bytes())
+
+		trimmed := strings.TrimSpace(exprText)
+		// Wrap the attribute with jsonencode
+		if len(trimmed) > 0 && trimmed[0] == '{' {
+			body.RemoveAttribute(attributeName)
+			newTokens := hclwrite.Tokens{}
+			fnStart := &hclwrite.Token{
+				Type:  hclsyntax.TokenIdent,
+				Bytes: []byte("jsonencode("),
+			}
+			newTokens = append(newTokens, fnStart)
+			newTokens = append(newTokens, exprTokens...)
+			fnEnd := &hclwrite.Token{
+				Type:  hclsyntax.TokenCParen,
+				Bytes: []byte(")"),
+			}
+			newTokens = append(newTokens, fnEnd)
+			body.SetAttributeRaw(attributeName, newTokens)
+		}
+	}
 }

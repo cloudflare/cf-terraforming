bbdeb2af7b6d6d21f75db4cbe567528591c6f71e
Vaishak Dinesh <vaishak@cloudflare.com>
2025-03-13 10:54:41 -0700

    fix: stream live input

--- testdata/terraform/v5/cloudflare_stream_live_input/test.tf
+++ testdata/terraform/v5/cloudflare_stream_live_input/test.tf
@@ -1,15 +1,7 @@
 resource "cloudflare_stream_live_input" "terraform_managed_resource" {
-  account_id = "023e105f4ecef8ad9ca31a8372d0c353"
-  default_creator = "defaultCreator"
-  delete_recording_after_days = 45
-  meta = {
+  account_id = "f037e56e89293a057740de681ac9abbe"
+  meta = jsonencode({
     name = "test stream 1"
-  }
-  recording = {
-    allowed_origins = ["example.com"]
-    hide_live_viewer_count = false
-    mode = "off"
-    require_signed_urls = false
-    timeout_seconds = 0
-  }
+  })
 }
+

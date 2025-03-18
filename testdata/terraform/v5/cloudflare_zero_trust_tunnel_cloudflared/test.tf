eae2e60597d6d2dbbd8ff5393bd6a68f63601104
Vaishak Dinesh <vaishak@cloudflare.com>
2025-03-18 09:55:42 -0700

    fix: zt tunnel cfd

--- testdata/terraform/v5/cloudflare_zero_trust_tunnel_cloudflared/test.tf
+++ testdata/terraform/v5/cloudflare_zero_trust_tunnel_cloudflared/test.tf
@@ -1,6 +1,15 @@
-resource "cloudflare_zero_trust_tunnel_cloudflared" "terraform_managed_resource" {
-  account_id = "699d98642c564d2e855e9661899b7252"
-  name = "blog"
-  config_src = "local"
-  tunnel_secret = "AQIDBAUGBwgBAgMEBQYHCAECAwQFBgcIAQIDBAUGBwg="
+resource "cloudflare_zero_trust_tunnel_cloudflared" "terraform_managed_resource_0" {
+  account_id = "f037e56e89293a057740de681ac9abbe"
+  name       = "wctzrdnxht"
 }
+
+resource "cloudflare_zero_trust_tunnel_cloudflared" "terraform_managed_resource_1" {
+  account_id = "f037e56e89293a057740de681ac9abbe"
+  name       = "kqvpdktike"
+}
+
+resource "cloudflare_zero_trust_tunnel_cloudflared" "terraform_managed_resource_2" {
+  account_id = "f037e56e89293a057740de681ac9abbe"
+  name       = "xiovjntpkl"
+}
+

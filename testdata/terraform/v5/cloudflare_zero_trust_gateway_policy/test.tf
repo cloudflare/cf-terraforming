bb8e154fa681648fc031c74e296f41d68d457b93
Vaishak Dinesh <vaishak@cloudflare.com>
2025-03-18 11:56:12 -0700

    fix: zt gateway policy

--- testdata/terraform/v5/cloudflare_zero_trust_gateway_policy/test.tf
+++ testdata/terraform/v5/cloudflare_zero_trust_gateway_policy/test.tf
@@ -1,105 +1,58 @@
-resource "cloudflare_zero_trust_gateway_policy" "terraform_managed_resource" {
-  account_id = "699d98642c564d2e855e9661899b7252"
-  action = "on"
-  name = "block bad websites"
-  description = "Block bad websites based on their host name."
-  device_posture = "any(device_posture.checks.passed[*] in {\"1308749e-fcfb-4ebc-b051-fe022b632644\"})"
-  enabled = true
-  expiration = {
-    expires_at = "2014-01-01T05:20:20Z"
-    duration = 10
-    expired = false
-  }
-  filters = ["http"]
-  identity = "any(identity.groups.name[*] in {\"finance\"})"
-  precedence = 0
+resource "cloudflare_zero_trust_gateway_policy" "terraform_managed_resource_0" {
+  account_id  = "f037e56e89293a057740de681ac9abbe"
+  action      = "block"
+  description = "desc"
+  enabled     = false
+  filters     = ["dns"]
+  name        = "rytdytpfmz"
+  precedence  = 12302
+  traffic     = "any(dns.domains[*] == \"example.com\")"
   rule_settings = {
-    add_headers = {
-      foo = "string"
-    }
-    allow_child_bypass = false
-    audit_ssh = {
-      command_logging = false
-    }
-    biso_admin_controls = {
-      copy = "enabled"
-      dcp = false
-      dd = false
-      dk = false
-      download = "enabled"
-      dp = false
-      du = false
-      keyboard = "enabled"
-      paste = "enabled"
-      printing = "enabled"
-      upload = "enabled"
-      version = "v1"
-    }
-    block_page_enabled = true
-    block_reason = "This website is a security risk"
-    bypass_parent_rule = false
-    check_session = {
-      duration = "300s"
-      enforce = true
-    }
-    dns_resolvers = {
-      ipv4 = [{
-        ip = "2.2.2.2"
-        port = 5053
-        route_through_private_network = true
-        vnet_id = "f174e90a-fafe-4643-bbbc-4a0ed4fc8415"
-      }]
-      ipv6 = [{
-        ip = "2001:DB8::"
-        port = 5053
-        route_through_private_network = true
-        vnet_id = "f174e90a-fafe-4643-bbbc-4a0ed4fc8415"
-      }]
-    }
+    add_headers         = null
+    biso_admin_controls = null
+    block_page_enabled  = true
+    block_reason        = "cuz"
+    check_session       = null
     egress = {
-      ipv4 = "192.0.2.2"
-      ipv4_fallback = "192.0.2.3"
-      ipv6 = "2001:DB8::/64"
+      ipv4 = "203.0.113.1"
+      ipv6 = "2001:db8::/32"
     }
-    ignore_cname_category_matches = true
     insecure_disable_dnssec_validation = false
-    ip_categories = true
-    ip_indicator_feeds = true
-    l4override = {
-      ip = "1.1.1.1"
-      port = 0
-    }
-    notification_settings = {
-      enabled = true
-      msg = "msg"
-      support_url = "support_url"
-    }
-    override_host = "example.com"
-    override_ips = ["1.1.1.1", "2.2.2.2"]
+    ip_categories                      = false
+    ip_indicator_feeds                 = false
+    l4override                         = null
+    override_host                      = ""
+    override_ips                       = null
     payload_log = {
       enabled = true
     }
-    quarantine = {
-      file_types = ["exe"]
-    }
-    resolve_dns_internally = {
-      fallback = "none"
-      view_id = "view_id"
-    }
-    resolve_dns_through_cloudflare = true
     untrusted_cert = {
-      action = "pass_through"
+      action = "error"
     }
   }
-  schedule = {
-    fri = "08:00-12:30,13:30-17:00"
-    mon = "08:00-12:30,13:30-17:00"
-    sat = "08:00-12:30,13:30-17:00"
-    sun = "08:00-12:30,13:30-17:00"
-    thu = "08:00-12:30,13:30-17:00"
-    time_zone = "America/New York"
-    tue = "08:00-12:30,13:30-17:00"
-    wed = "08:00-12:30,13:30-17:00"
-  }
-  traffic = "http.request.uri matches \".*a/partial/uri.*\" and http.request.host in $01302951-49f9-47c9-a400-0297e60b6a10"
 }
+
+resource "cloudflare_zero_trust_gateway_policy" "terraform_managed_resource_1" {
+  account_id  = "f037e56e89293a057740de681ac9abbe"
+  action      = "block"
+  description = "test description "
+  enabled     = true
+  filters     = ["http"]
+  name        = "test-http"
+  precedence  = 13302
+  traffic     = "http.request.uri.path == \"/foo\""
+  rule_settings = {
+    add_headers                        = null
+    biso_admin_controls                = null
+    block_page_enabled                 = false
+    block_reason                       = ""
+    check_session                      = null
+    insecure_disable_dnssec_validation = false
+    ip_categories                      = false
+    ip_indicator_feeds                 = false
+    l4override                         = null
+    override_host                      = ""
+    override_ips                       = null
+  }
+}
+

520f097574e77fc1216d57b7537039128c8b5b54
Vaishak Dinesh <vaishak@cloudflare.com>
2025-03-18 11:56:00 -0700

    fix: zt gateway cert

--- testdata/terraform/v5/cloudflare_zero_trust_gateway_certificate/test.tf
+++ testdata/terraform/v5/cloudflare_zero_trust_gateway_certificate/test.tf
@@ -1,4 +1,8 @@
-resource "cloudflare_zero_trust_gateway_certificate" "terraform_managed_resource" {
-  account_id = "699d98642c564d2e855e9661899b7252"
-  validity_period_days = 1826
+resource "cloudflare_zero_trust_gateway_certificate" "terraform_managed_resource_0" {
+  account_id = "f037e56e89293a057740de681ac9abbe"
 }
+
+resource "cloudflare_zero_trust_gateway_certificate" "terraform_managed_resource_1" {
+  account_id = "f037e56e89293a057740de681ac9abbe"
+}
+

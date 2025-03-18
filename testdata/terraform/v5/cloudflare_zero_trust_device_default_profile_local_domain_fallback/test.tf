ae19c568c29967911de16f62da7ed689e2fb9160
Vaishak Dinesh <vaishak@cloudflare.com>
2025-03-18 11:55:13 -0700

    fix: cloudflare zero trust device default profile local domain fallback

--- testdata/terraform/v5/cloudflare_zero_trust_device_default_profile_local_domain_fallback/test.tf
+++ testdata/terraform/v5/cloudflare_zero_trust_device_default_profile_local_domain_fallback/test.tf
@@ -1,8 +1,18 @@
-resource "cloudflare_zero_trust_device_default_profile_local_domain_fallback" "terraform_managed_resource" {
-  account_id = "699d98642c564d2e855e9661899b7252"
+resource "cloudflare_zero_trust_device_default_profile_local_domain_fallback" "terraform_managed_resource_0" {
+  account_id = "f037e56e89293a057740de681ac9abbe"
   domains = [{
-    suffix = "example.com"
-    description = "Domain bypass for local development"
-    dns_server = ["1.1.1.1"]
+    description = "example domain"
+    dns_server  = ["1.0.0.1"]
+    suffix      = "example.com"
   }]
 }
+
+resource "cloudflare_zero_trust_device_default_profile_local_domain_fallback" "terraform_managed_resource_1" {
+  account_id = "f037e56e89293a057740de681ac9abbe"
+  domains = [{
+    description = "foo domain"
+    dns_server  = ["1.0.0.1"]
+    suffix      = "foo.com"
+  }]
+}
+

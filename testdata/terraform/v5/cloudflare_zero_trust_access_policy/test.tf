2258e178200a69dc8bf0f22ac241bb55d6950e50
Vaishak Dinesh <vaishak@cloudflare.com>
2025-03-18 09:54:43 -0700

    fix: zt access policy

--- testdata/terraform/v5/cloudflare_zero_trust_access_policy/test.tf
+++ testdata/terraform/v5/cloudflare_zero_trust_access_policy/test.tf
@@ -1,34 +1,28 @@
-resource "cloudflare_zero_trust_access_policy" "terraform_managed_resource" {
-  account_id = "023e105f4ecef8ad9ca31a8372d0c353"
-  decision = "allow"
-  include = [{
-    group = {
-      id = "aa0a4aab-672b-4bdb-bc33-a59f1130a11f"
-    }
-  }]
-  name = "Allow devs"
-  approval_groups = [{
-    approvals_needed = 1
-    email_addresses = ["test1@cloudflare.com", "test2@cloudflare.com"]
-    email_list_uuid = "email_list_uuid"
-  }, {
-    approvals_needed = 3
-    email_addresses = ["test@cloudflare.com", "test2@cloudflare.com"]
-    email_list_uuid = "597147a1-976b-4ef2-9af0-81d5d007fc34"
-  }]
-  approval_required = true
-  exclude = [{
-    group = {
-      id = "aa0a4aab-672b-4bdb-bc33-a59f1130a11f"
-    }
-  }]
-  isolation_required = false
-  purpose_justification_prompt = "Please enter a justification for entering this protected domain."
-  purpose_justification_required = true
-  require = [{
-    group = {
-      id = "aa0a4aab-672b-4bdb-bc33-a59f1130a11f"
-    }
-  }]
+resource "cloudflare_zero_trust_access_policy" "terraform_managed_resource_0" {
+  account_id       = "f037e56e89293a057740de681ac9abbe"
+  decision         = "non_identity"
+  name             = "dryjkmkfpz"
   session_duration = "24h"
+  exclude          = []
+  include = [{
+    ip = {
+      ip = "127.0.0.1/32"
+    }
+  }]
+  require = []
 }
+
+resource "cloudflare_zero_trust_access_policy" "terraform_managed_resource_1" {
+  account_id       = "f037e56e89293a057740de681ac9abbe"
+  decision         = "non_identity"
+  name             = "dryjkmkfpz"
+  session_duration = "24h"
+  exclude          = []
+  include = [{
+    ip = {
+      ip = "127.0.0.1/32"
+    }
+  }]
+  require = []
+}
+

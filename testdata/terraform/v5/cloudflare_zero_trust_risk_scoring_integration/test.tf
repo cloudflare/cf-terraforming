ae4031f06e86eae59182212e141c9973e5462683
Vaishak Dinesh <vaishak@cloudflare.com>
2025-03-20 08:37:32 -0700

    fix: zt risk scoring integration

--- testdata/terraform/v5/cloudflare_zero_trust_risk_scoring_integration/test.tf
+++ testdata/terraform/v5/cloudflare_zero_trust_risk_scoring_integration/test.tf
@@ -1,6 +1,8 @@
 resource "cloudflare_zero_trust_risk_scoring_integration" "terraform_managed_resource" {
-  account_id = "account_id"
+  account_id       = "f037e56e89293a057740de681ac9abbe"
+  active           = true
   integration_type = "Okta"
-  tenant_url = "https://example.com"
-  reference_id = "reference_id"
+  reference_id     = "3701e70b-5de4-4d9d-8972-f5aa994d9057"
+  tenant_url       = "https://example.oktapreview.com"
 }
+

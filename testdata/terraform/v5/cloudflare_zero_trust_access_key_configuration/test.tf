a05faf7ff12bd32dd382f262acf0be9a46aa8a1f
Vaishak Dinesh <vaishak@cloudflare.com>
2025-03-20 13:48:41 -0700

    fix: zt access key config

--- testdata/terraform/v5/cloudflare_zero_trust_access_key_configuration/test.tf
+++ testdata/terraform/v5/cloudflare_zero_trust_access_key_configuration/test.tf
@@ -1,4 +1,5 @@
 resource "cloudflare_zero_trust_access_key_configuration" "terraform_managed_resource" {
-  account_id = "023e105f4ecef8ad9ca31a8372d0c353"
-  key_rotation_interval_days = 30
+  account_id                 = "f037e56e89293a057740de681ac9abbe"
+  key_rotation_interval_days = 60
 }
+

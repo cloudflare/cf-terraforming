91ea0b16c9d286d82c6fa679f27d084578077c96
Vaishak Dinesh <vaishak@cloudflare.com>
2025-03-20 08:37:14 -0700

    fix: zt gateway proxy endpoint

--- testdata/terraform/v5/cloudflare_zero_trust_gateway_proxy_endpoint/test.tf
+++ testdata/terraform/v5/cloudflare_zero_trust_gateway_proxy_endpoint/test.tf
@@ -1,5 +1,6 @@
 resource "cloudflare_zero_trust_gateway_proxy_endpoint" "terraform_managed_resource" {
-  account_id = "699d98642c564d2e855e9661899b7252"
-  ips = ["192.0.2.1/32"]
-  name = "Devops team"
+  account_id = "f037e56e89293a057740de681ac9abbe"
+  ips        = ["104.16.132.229/32"]
+  name       = "msmfnzqoqa"
 }
+

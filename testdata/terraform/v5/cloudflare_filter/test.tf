aa2f7dbabe39418282cda82dd22313f3d26b8f8a
Vaishak Dinesh <vaishak@cloudflare.com>
2025-03-20 10:59:07 -0700

    fix: filter

--- testdata/terraform/v5/cloudflare_filter/test.tf
+++ testdata/terraform/v5/cloudflare_filter/test.tf
@@ -1,4 +1,5 @@
 resource "cloudflare_filter" "terraform_managed_resource" {
-  zone_id = "023e105f4ecef8ad9ca31a8372d0c353"
-  expression = "(http.request.uri.path ~ \".*wp-login.php\" or http.request.uri.path ~ \".*xmlrpc.php\") and ip.addr ne 172.16.22.155"
+  expression = "(http.request.uri.path eq \"/hello\")"
+  zone_id    = "0da42c8d2132a9ddaf714f9e7c920711"
 }
+

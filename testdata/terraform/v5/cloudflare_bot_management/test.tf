eb11ddd9b0d1eac9672ba40c03a106a1cee371a6
Vaishak Dinesh <vaishak@cloudflare.com>
2025-03-20 14:07:25 -0700

    fix: bot management

--- testdata/terraform/v5/cloudflare_bot_management/test.tf
+++ testdata/terraform/v5/cloudflare_bot_management/test.tf
@@ -1,6 +1,12 @@
 resource "cloudflare_bot_management" "terraform_managed_resource" {
-  zone_id = "023e105f4ecef8ad9ca31a8372d0c353"
-  ai_bots_protection = "block"
-  enable_js = true
-  fight_mode = true
+  ai_bots_protection              = "disabled"
+  enable_js                       = true
+  optimize_wordpress              = true
+  sbfm_definitely_automated       = "managed_challenge"
+  sbfm_likely_automated           = "block"
+  sbfm_static_resource_protection = false
+  sbfm_verified_bots              = "allow"
+  suppress_session_score          = false
+  zone_id                         = "0da42c8d2132a9ddaf714f9e7c920711"
 }
+

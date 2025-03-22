4cc293e2b18826e4da2160640b128a6763856ed6
Vaishak Dinesh <vaishak@cloudflare.com>
2025-03-21 17:35:37 -0700

    chore: testdata

--- testdata/terraform/v5/cloudflare_zone_setting/test.tf
+++ testdata/terraform/v5/cloudflare_zone_setting/test.tf
@@ -1,6 +1,12 @@
-resource "cloudflare_zone_setting" "terraform_managed_resource" {
-  zone_id = "023e105f4ecef8ad9ca31a8372d0c353"
+resource "cloudflare_zone_setting" "terraform_managed_resource_0" {
   setting_id = "always_online"
-  id = "0rtt"
-  value = "on"
+  zone_id    = "0da42c8d2132a9ddaf714f9e7c920711"
+  value      = "off"
 }
+
+resource "cloudflare_zone_setting" "terraform_managed_resource_1" {
+  setting_id = "cache_level"
+  zone_id    = "0da42c8d2132a9ddaf714f9e7c920711"
+  value      = "aggressive"
+}
+

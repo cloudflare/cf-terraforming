ab58e11c08363afb0fd71ae959dde86e543a6287
Vaishak Dinesh <vaishak@cloudflare.com>
2025-04-11 08:30:49 -0700

    fix: email security trusted domains

--- testdata/terraform/v5/cloudflare_email_security_trusted_domains/test.tf
+++ testdata/terraform/v5/cloudflare_email_security_trusted_domains/test.tf
@@ -1,8 +1,8 @@
 resource "cloudflare_email_security_trusted_domains" "terraform_managed_resource" {
-  account_id = "023e105f4ecef8ad9ca31a8372d0c353"
-  is_recent = true
-  is_regex = false
+  account_id    = "f037e56e89293a057740de681ac9abbe"
+  is_recent     = true
+  is_regex      = false
   is_similarity = false
-  pattern = "example.com"
-  comments = null
+  pattern       = "example.com"
 }
+

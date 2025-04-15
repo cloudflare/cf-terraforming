6322554f8f4dc1a08fc801e6715b33a9979ad1f6
Vaishak Dinesh <vaishak@cloudflare.com>
2025-04-15 11:35:16 -0700

    fix: api shield

--- testdata/terraform/v5/cloudflare_api_shield_discovery_operation/test.tf
+++ testdata/terraform/v5/cloudflare_api_shield_discovery_operation/test.tf
@@ -1,5 +1,12 @@
-resource "cloudflare_api_shield_discovery_operation" "terraform_managed_resource" {
-  zone_id = "023e105f4ecef8ad9ca31a8372d0c353"
-  operation_id = "f174e90a-fafe-4643-bbbc-4a0ed4fc8415"
-  state = "review"
+resource "cloudflare_api_shield_discovery_operation" "terraform_managed_resource_0" {
+  operation_id = "38e124f5-fb1c-4235-a726-9471e1bd0161"
+  state        = "review"
+  zone_id      = "0da42c8d2132a9ddaf714f9e7c920711"
 }
+
+resource "cloudflare_api_shield_discovery_operation" "terraform_managed_resource_1" {
+  operation_id = "1299ce1a-4f00-4ae7-bed1-658df4369f97"
+  state        = "ignored"
+  zone_id      = "0da42c8d2132a9ddaf714f9e7c920711"
+}
+
--- testdata/terraform/v5/cloudflare_api_shield_schema/test.tf
+++ testdata/terraform/v5/cloudflare_api_shield_schema/test.tf
@@ -1,7 +1,18 @@
-resource "cloudflare_api_shield_schema" "terraform_managed_resource" {
-  zone_id = "023e105f4ecef8ad9ca31a8372d0c353"
-  file = "file.txt"
-  kind = "openapi_v3"
-  name = "petstore schema"
-  validation_enabled = "true"
+resource "cloudflare_api_shield_schema" "terraform_managed_resource_0" {
+  file               = "{\"info\":{\"title\":\"Example\",\"version\":\"0.1.0\"},\"openapi\":\"3.0.3\",\"paths\":{\"/\":{}},\"servers\":[{\"url\":\"api.example.com\"}]}"
+  kind               = "openapi_v3"
+  name               = "example_schema.json"
+  schema_id          = "59f6e0a9-7d8d-446f-b4c8-fb9c2c1abae8"
+  validation_enabled = true
+  zone_id            = "0da42c8d2132a9ddaf714f9e7c920711"
 }
+
+resource "cloudflare_api_shield_schema" "terraform_managed_resource_1" {
+  file               = "{\"info\":{\"title\":\"Example\",\"version\":\"0.1.0\"},\"openapi\":\"3.0.3\",\"paths\":{\"/\":{}},\"servers\":[{\"url\":\"api.example.com\"}]}"
+  kind               = "openapi_v3"
+  name               = "example_schema.json"
+  schema_id          = "fef87c1a-6ff7-4d3a-aeee-fe6cf9ca948a"
+  validation_enabled = true
+  zone_id            = "0da42c8d2132a9ddaf714f9e7c920711"
+}
+

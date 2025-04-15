0ccc5f2f9cd077f34c060916c7e03b98e1716767
Vaishak Dinesh <vaishak@cloudflare.com>
2025-04-15 09:41:29 -0700

    fix: pages domain

--- testdata/terraform/v5/cloudflare_pages_domain/test.tf
+++ testdata/terraform/v5/cloudflare_pages_domain/test.tf
@@ -1,5 +1,6 @@
 resource "cloudflare_pages_domain" "terraform_managed_resource" {
-  account_id = "023e105f4ecef8ad9ca31a8372d0c353"
-  project_name = "this-is-my-project-01"
-  name = "example.com"
+  account_id   = "f037e56e89293a057740de681ac9abbe"
+  name         = "ykfjmcgpfs.terraform.cfapi.net"
+  project_name = "ykfjmcgpfs"
 }
+

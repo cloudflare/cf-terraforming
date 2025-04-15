48fab82da5f9933b902007effad096b2e4b04097
Vaishak Dinesh <vaishak@cloudflare.com>
2025-04-15 09:41:46 -0700

    fix: r2 custom domain

--- testdata/terraform/v5/cloudflare_r2_custom_domain/test.tf
+++ testdata/terraform/v5/cloudflare_r2_custom_domain/test.tf
@@ -1,8 +1,32 @@
-resource "cloudflare_r2_custom_domain" "terraform_managed_resource" {
-  account_id = "023e105f4ecef8ad9ca31a8372d0c353"
-  bucket_name = "example-bucket"
-  domain = "prefix.example-domain.com"
-  enabled = true
-  zone_id = "36ca64a6d92827b8a6b90be344bb1bfd"
-  min_tls = "1.0"
+resource "cloudflare_r2_custom_domain" "terraform_managed_resource_0" {
+  account_id  = "f037e56e89293a057740de681ac9abbe"
+  bucket_name = "jb-test-bucket"
+  domain      = "terraform2.cfapi.net"
+  enabled     = true
+  zone_id     = "b72110c08e3382597095c29ba7e661ea"
 }
+
+resource "cloudflare_r2_custom_domain" "terraform_managed_resource_1" {
+  account_id  = "f037e56e89293a057740de681ac9abbe"
+  bucket_name = "jb-test-bucket"
+  domain      = "terraform32.cfapi.net"
+  enabled     = true
+  zone_id     = "b72110c08e3382597095c29ba7e661ea"
+}
+
+resource "cloudflare_r2_custom_domain" "terraform_managed_resource_2" {
+  account_id  = "f037e56e89293a057740de681ac9abbe"
+  bucket_name = "bnfywlzwpt"
+  domain      = "terraform2.cfapi.net"
+  enabled     = true
+  zone_id     = "b72110c08e3382597095c29ba7e661ea"
+}
+
+resource "cloudflare_r2_custom_domain" "terraform_managed_resource_3" {
+  account_id  = "f037e56e89293a057740de681ac9abbe"
+  bucket_name = "bnfywlzwpt"
+  domain      = "terraform22.cfapi.net"
+  enabled     = true
+  zone_id     = "b72110c08e3382597095c29ba7e661ea"
+}
+

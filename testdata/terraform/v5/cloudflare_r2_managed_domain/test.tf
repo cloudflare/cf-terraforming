aa83c3937a98b1402e83c4a4883a60370a85d62e
Vaishak Dinesh <vaishak@cloudflare.com>
2025-04-15 09:41:55 -0700

    fix: r2 managed domain

--- testdata/terraform/v5/cloudflare_r2_managed_domain/test.tf
+++ testdata/terraform/v5/cloudflare_r2_managed_domain/test.tf
@@ -1,5 +1,12 @@
-resource "cloudflare_r2_managed_domain" "terraform_managed_resource" {
-  account_id = "023e105f4ecef8ad9ca31a8372d0c353"
-  bucket_name = "example-bucket"
-  enabled = true
+resource "cloudflare_r2_managed_domain" "terraform_managed_resource_0" {
+  account_id  = "f037e56e89293a057740de681ac9abbe"
+  bucket_name = "jb-test-bucket"
+  enabled     = false
 }
+
+resource "cloudflare_r2_managed_domain" "terraform_managed_resource_1" {
+  account_id  = "f037e56e89293a057740de681ac9abbe"
+  bucket_name = "bnfywlzwpt"
+  enabled     = false
+}
+

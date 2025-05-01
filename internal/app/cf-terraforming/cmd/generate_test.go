58caefb5f8704df885313718c4af35e518c09e53
Vaishak Dinesh <vaishak@cloudflare.com>
2025-05-01 12:05:03 -0700

    chore: comment

--- internal/app/cf-terraforming/cmd/generate_test.go
+++ internal/app/cf-terraforming/cmd/generate_test.go
@@ -250,7 +250,7 @@ func TestResourceGeneration(t *testing.T) {
 }
 
 func TestResourceGenerationV5(t *testing.T) {
-	//t.Skip("skip until the v5 provider is fully supported")
+	t.Skip("skip until the v5 provider is fully supported")
 
 	tests := map[string]struct {
 		identiferType    string

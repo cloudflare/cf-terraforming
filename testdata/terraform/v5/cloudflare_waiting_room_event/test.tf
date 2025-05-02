a09bb584c7fe01834ef4a437265b6f2d54b567f4
Vaishak Dinesh <vaishak@cloudflare.com>
2025-05-02 11:02:52 -0700

    fix: waiting room events

--- testdata/terraform/v5/cloudflare_waiting_room_event/test.tf
+++ testdata/terraform/v5/cloudflare_waiting_room_event/test.tf
@@ -1,17 +1,14 @@
 resource "cloudflare_waiting_room_event" "terraform_managed_resource" {
-  zone_id = "023e105f4ecef8ad9ca31a8372d0c353"
-  waiting_room_id = "699d98642c564d2e855e9661899b7252"
-  event_end_time = "2021-09-28T17:00:00.000Z"
-  event_start_time = "2021-09-28T15:30:00.000Z"
-  name = "production_webinar_event"
-  custom_page_html = "{{#waitTimeKnown}} {{waitTime}} mins {{/waitTimeKnown}} {{^waitTimeKnown}} Event is prequeueing / Queue all enabled {{/waitTimeKnown}}"
-  description = "Production event - DO NOT MODIFY"
-  disable_session_renewal = true
-  new_users_per_minute = 200
-  prequeue_start_time = "2021-09-28T15:00:00.000Z"
-  queueing_method = "random"
-  session_duration = 1
-  shuffle_at_event_start = true
-  suspended = true
-  total_active_users = 200
+  custom_page_html       = "{{#waitTimeKnown}} {{waitTime}} mins {{/waitTimeKnown}} {{^waitTimeKnown}} Event is prequeueing / Queue all enabled {{/waitTimeKnown}}"
+  description            = "Production event - DO NOT MODIFY"
+  event_end_time         = "2021-09-28T17:00:00Z"
+  event_start_time       = "2021-09-28T15:30:00Z"
+  name                   = "production_webinar_event"
+  prequeue_start_time    = "2021-09-28T15:00:00Z"
+  queueing_method        = "random"
+  shuffle_at_event_start = false
+  suspended              = false
+  waiting_room_id        = "e7f9e4c190ea8d6c66cab32ac110f39a"
+  zone_id                = "0da42c8d2132a9ddaf714f9e7c920711"
 }
+

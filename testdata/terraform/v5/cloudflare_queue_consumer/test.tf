2059696509ca94db0d620f93a7050a8a6f62f1f2
Vaishak Dinesh <vaishak@cloudflare.com>
2025-06-10 14:54:15 -0700

    fix: queue consumer

--- testdata/terraform/v5/cloudflare_queue_consumer/test.tf
+++ testdata/terraform/v5/cloudflare_queue_consumer/test.tf
@@ -1,14 +1,14 @@
 resource "cloudflare_queue_consumer" "terraform_managed_resource" {
-  account_id = "023e105f4ecef8ad9ca31a8372d0c353"
-  queue_id = "023e105f4ecef8ad9ca31a8372d0c353"
-  dead_letter_queue = "example-queue"
-  script_name = "my-consumer-worker"
+  account_id  = "f037e56e89293a057740de681ac9abbe"
+  consumer_id = "2f4e3428eaa0472bb6954cf6b7fb932f"
+  queue_id    = "2dde6ac405cd457c9ce59dc4bda20c65"
+  type        = "worker"
   settings = {
-    batch_size = 50
-    max_concurrency = 10
-    max_retries = 3
+    batch_size       = 50
+    max_concurrency  = 10
+    max_retries      = 5
     max_wait_time_ms = 5000
-    retry_delay = 10
+    retry_delay      = 10
   }
-  type = "worker"
 }
+

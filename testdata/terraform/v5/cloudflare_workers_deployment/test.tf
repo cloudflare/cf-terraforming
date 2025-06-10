3bf7f6a98426dabf61b09de57af81b425748963d
Vaishak Dinesh <vaishak@cloudflare.com>
2025-06-10 14:54:44 -0700

    fix: workers deployment

--- testdata/terraform/v5/cloudflare_workers_deployment/test.tf
+++ testdata/terraform/v5/cloudflare_workers_deployment/test.tf
@@ -1,12 +1,28 @@
-resource "cloudflare_workers_deployment" "terraform_managed_resource" {
-  account_id = "023e105f4ecef8ad9ca31a8372d0c353"
-  script_name = "this-is_my_script-01"
-  strategy = "percentage"
+resource "cloudflare_workers_deployment" "terraform_managed_resource_0" {
+  account_id  = "f037e56e89293a057740de681ac9abbe"
+  script_name = "script_2"
+  strategy    = "percentage"
+  annotations = {
+    "workers/message"      = "Automatic deployment on upload."
+    "workers/triggered_by" = "upload"
+  }
   versions = [{
     percentage = 100
-    version_id = "bcf48806-b317-4351-9ee7-36e7d557d4de"
+    version_id = "a81e19c8-2d06-4495-b4dd-7fcbf7729a86"
   }]
-  annotations = {
-    workers_message = "Deploy bug fix."
-  }
 }
+
+resource "cloudflare_workers_deployment" "terraform_managed_resource_1" {
+  account_id  = "f037e56e89293a057740de681ac9abbe"
+  script_name = "script_2"
+  strategy    = "percentage"
+  annotations = {
+    "workers/message"      = "Automatic deployment on upload."
+    "workers/triggered_by" = "upload"
+  }
+  versions = [{
+    percentage = 100
+    version_id = "a81e19c8-2d06-4495-b4dd-7fcbf7729a86"
+  }]
+}
+

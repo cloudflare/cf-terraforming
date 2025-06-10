0c245cd3e4e753daab5ff3b53f2fd862cd8ecdd5
Vaishak Dinesh <vaishak@cloudflare.com>
2025-06-10 14:52:39 -0700

    fix: api shield schema validation setting

--- testdata/terraform/v5/cloudflare_api_shield_schema_validation_settings/test.tf
+++ testdata/terraform/v5/cloudflare_api_shield_schema_validation_settings/test.tf
@@ -1,5 +1,5 @@
 resource "cloudflare_api_shield_schema_validation_settings" "terraform_managed_resource" {
-  zone_id = "023e105f4ecef8ad9ca31a8372d0c353"
-  validation_default_mitigation_action = "none"
-  validation_override_mitigation_action = "none"
+  validation_default_mitigation_action = "log"
+  zone_id                              = "0da42c8d2132a9ddaf714f9e7c920711"
 }
+

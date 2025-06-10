a9a2f76d49f2ef5b60258a4a29cafa14fe8b6f2c
Vaishak Dinesh <vaishak@cloudflare.com>
2025-06-10 14:55:24 -0700

    fix: zt access mtls

--- testdata/terraform/v5/cloudflare_zero_trust_access_mtls_hostname_settings/test.tf
+++ testdata/terraform/v5/cloudflare_zero_trust_access_mtls_hostname_settings/test.tf
@@ -1,8 +1,9 @@
 resource "cloudflare_zero_trust_access_mtls_hostname_settings" "terraform_managed_resource" {
+  account_id = "f037e56e89293a057740de681ac9abbe"
   settings = [{
-    china_network = false
+    china_network                 = false
     client_certificate_forwarding = true
-    hostname = "admin.example.com"
+    hostname                      = "terraform.cfapi.net"
   }]
-  zone_id = "zone_id"
 }
+

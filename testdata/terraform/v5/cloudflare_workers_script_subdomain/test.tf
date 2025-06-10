38cee816df30243ee65b6cb4b6a03f9a525a569e
Vaishak Dinesh <vaishak@cloudflare.com>
2025-06-10 14:55:01 -0700

    fix: workers script subdomain

--- testdata/terraform/v5/cloudflare_workers_script_subdomain/test.tf
+++ testdata/terraform/v5/cloudflare_workers_script_subdomain/test.tf
@@ -1,6 +1,7 @@
 resource "cloudflare_workers_script_subdomain" "terraform_managed_resource" {
-  account_id = "023e105f4ecef8ad9ca31a8372d0c353"
-  script_name = "this-is_my_script-01"
-  enabled = true
+  account_id       = "f037e56e89293a057740de681ac9abbe"
+  enabled          = true
   previews_enabled = true
+  script_name      = "accounts"
 }
+

0599e5ced2e898b3ae76506f475b8fbdaabcee51
Vaishak Dinesh <vaishak@cloudflare.com>
2025-06-10 14:53:58 -0700

    fix: cloudflare_observatory_scheduled_test

--- testdata/terraform/v5/cloudflare_observatory_scheduled_test/test.tf
+++ testdata/terraform/v5/cloudflare_observatory_scheduled_test/test.tf
@@ -1,4 +1,5 @@
 resource "cloudflare_observatory_scheduled_test" "terraform_managed_resource" {
-  zone_id = "023e105f4ecef8ad9ca31a8372d0c353"
-  url = "example.com"
+  zone_id = "0da42c8d2132a9ddaf714f9e7c920711"
+  url     = urlencode("terraform.cfapi.net/thyygxveip")
 }
+

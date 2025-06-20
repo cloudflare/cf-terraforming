1c2784c6874e344faaa68d450df0332b1a943525
Vaishak Dinesh <vaishak@cloudflare.com>
2025-06-19 17:39:53 -0700

    fix: testdata

--- testdata/terraform/v5/cloudflare_account_member/test.tf
+++ testdata/terraform/v5/cloudflare_account_member/test.tf
@@ -12,3 +12,10 @@ resource "cloudflare_account_member" "terraform_managed_resource_1" {
   status     = "accepted"
 }
 
+resource "cloudflare_account_member" "terraform_managed_resource_2" {
+  account_id = "f037e56e89293a057740de681ac9abbe"
+  email      = "jdoe@cloudflare.com"
+  roles      = ["33666b9c79b9a5273fc7344ff42f953d"]
+  status     = "accepted"
+}
+

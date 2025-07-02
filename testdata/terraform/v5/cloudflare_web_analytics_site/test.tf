a4b223c29c69963d5ecedf5f75025ee5f1f1f57d
Vaishak Dinesh <vaishak@cloudflare.com>
2025-07-02 10:14:53 -0700

    chore: testdata

--- testdata/terraform/v5/cloudflare_web_analytics_site/test.tf
+++ testdata/terraform/v5/cloudflare_web_analytics_site/test.tf
@@ -1,5 +1,8 @@
 resource "cloudflare_web_analytics_site" "terraform_managed_resource" {
   account_id   = "f037e56e89293a057740de681ac9abbe"
   auto_install = true
+  enabled      = true
+  lite         = false
+  zone_tag     = "0da42c8d2132a9ddaf714f9e7c920711"
 }
 

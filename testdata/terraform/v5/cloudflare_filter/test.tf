7c6110607abb7dc4a5579595099a2cb5c68d68d8
rotem <rotem@cloudflare.com>
2026-02-17 17:10:28 -0600

    fix: testdata

--- testdata/terraform/v5/cloudflare_filter/test.tf
+++ testdata/terraform/v5/cloudflare_filter/test.tf
@@ -1,5 +1,6 @@
 resource "cloudflare_filter" "terraform_managed_resource" {
   expression = "(http.request.uri.path eq \"/hello\")"
+  paused     = false
   zone_id    = "0da42c8d2132a9ddaf714f9e7c920711"
 }
 
--- testdata/terraform/v5/cloudflare_zero_trust_access_custom_page/test.tf
+++ testdata/terraform/v5/cloudflare_zero_trust_access_custom_page/test.tf
@@ -1,6 +1,5 @@
 resource "cloudflare_zero_trust_access_custom_page" "terraform_managed_resource" {
   account_id  = "f037e56e89293a057740de681ac9abbe"
-  app_count   = 0
   custom_html = "<html><body><h1>Access Denied</h1></body></html>"
   name        = "plabknfrou"
   type        = "forbidden"

bc07265dab8ade3e74b1c6edab1ea18a8eec0e57
Sarah Sicard <ssicard@cloudflare.com>
2026-07-20 13:40:11 -0500

    fix(test): update impersonation_registry fixture for new schema fields

--- testdata/terraform/v5/cloudflare_email_security_impersonation_registry/test.tf
+++ testdata/terraform/v5/cloudflare_email_security_impersonation_registry/test.tf
@@ -1,7 +1,10 @@
 resource "cloudflare_email_security_impersonation_registry" "terraform_managed_resource" {
-  account_id     = "f037e56e89293a057740de681ac9abbe"
-  email          = "email"
-  is_email_regex = true
-  name           = "name"
+  account_id                 = "f037e56e89293a057740de681ac9abbe"
+  directory_id               = 0
+  email                      = "email"
+  external_directory_node_id = "12444788"
+  is_email_regex             = true
+  name                       = "name"
+  provenance                 = "A1S_INTERNAL"
 }
 
